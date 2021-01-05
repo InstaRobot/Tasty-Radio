b986c2446e6b51c1a0545182e374a17c4b1f087b
Vitaliy Podolskiy <instarobot.net@gmail.com>
2021-01-05 20:06:55 +0300

    Update CloudKitService.swift

--- Tasty Radio/Services/CloudKitService.swift
+++ Tasty Radio/Services/CloudKitService.swift
@@ -11,14 +11,16 @@ import CloudKit
 
 class CloudKitService {
     static let shared = CloudKitService()
-    
-    var stations = [RadioStation]()
-    
+
     private let recordFavourite = "FavouriteStation"
     private let recordRate = "RatedStation"
     
     private let privateCloudDatabase = CKContainer.default().publicCloudDatabase//.privateCloudDatabase
     
+    /// Сохранение станции в избранное
+    /// - Parameters:
+    ///   - station: модель станции
+    ///   - callback: выход по завершении
     func saveStationToCloud(station: RadioStation, callback: @escaping () -> Void) {
         let record = CKRecord(recordType: recordFavourite)
         record.setValue(station.name, forKey: "name")
@@ -37,8 +39,27 @@ class CloudKitService {
         }
     }
     
+    /// Сохранение оцененной станции в список ранее оцениваемых станций
+    /// - Parameters:
+    ///   - stationId: ид станции
+    ///   - callback: выход по готовности
+    func saveRatedToCloud(with stationId: String, callback: @escaping () -> Void) {
+        let record = CKRecord(recordType: recordRate)
+        record.setValue(stationId, forKey: "stationId")
+        
+        privateCloudDatabase.save(record) { (ckRecord, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            callback()
+        }
+    }
+    
+    /// Загрузка списка избранных станций для пользователя
+    /// - Parameter callback: массив моделей избранных станций
     func fetchStationsFromCloud(callback: @escaping ([RadioStation]) -> Void) {
-        self.stations = []
+        var stations = [RadioStation]()
         
         let query = CKQuery(recordType: recordFavourite, predicate: NSPredicate(value: true))
         query.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
@@ -49,7 +70,7 @@ class CloudKitService {
         
         queryOperation.recordFetchedBlock = { record in
             let station = RadioStation(record: record)
-            self.stations.append(station)
+            stations.append(station)
         }
         
         queryOperation.queryCompletionBlock = { _, error in
@@ -57,12 +78,38 @@ class CloudKitService {
                 print(error.localizedDescription)
                 return
             }
-            callback(self.stations)
+            callback(stations)
         }
         
         privateCloudDatabase.add(queryOperation)
     }
     
+    /// Загрузка ранее оцениваемых станций для пользователя
+    /// - Parameter callback: массив моделей оцениваемых станций
+    func fetchRatedFromCloud(callback: @escaping ([RatedStation]) -> Void) {
+        var rated: [RatedStation] = []
+        let query = CKQuery(recordType: recordRate, predicate: NSPredicate(value: true))
+        let queryOperation = CKQueryOperation(query: query)
+        
+        queryOperation.recordFetchedBlock = { record in
+            let station = RatedStation(record: record)
+            rated.append(station)
+        }
+        
+        queryOperation.queryCompletionBlock = { _, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            callback(rated)
+        }
+        privateCloudDatabase.add(queryOperation)
+    }
+    
+    /// Удаление станции из списка избранного для пользователя
+    /// - Parameters:
+    ///   - stationId: ид станции
+    ///   - callback: выход по готовности
     func deleteStation(with stationId: String, callback: @escaping () -> Void) {
         let query = CKQuery(
             recordType: recordFavourite,
