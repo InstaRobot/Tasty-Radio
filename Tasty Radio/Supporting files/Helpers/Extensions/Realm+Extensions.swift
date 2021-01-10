7fae0126609af7c0a4eff85e90002bc5b86e8824
Vitaliy Podolskiy <instarobot.net@gmail.com>
2021-01-10 19:31:12 +0300

    add realm extensions

---
+++ Tasty Radio/Supporting files/Helpers/Extensions/Realm+Extensions.swift
@@ -0,0 +1,120 @@
+//
+//  Realm+Extensions.swift
+//  Tasty Radio
+//
+//  Created by Vitaliy Podolskiy on 10.01.2021.
+//  Copyright © 2021 DEVLAB, LLC. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Realm
+
+final class RealmObjects {
+
+    /// Возвращаем объекты по принажлежности к определенной модели
+    ///
+    /// - Parameter type: передаем модель
+    /// - Returns: массив с выбранными моделями
+    static func objects<T: Object>(type: T.Type) -> Results<T>? {
+        let realm = try? Realm()
+        return realm?.objects(type)
+    }
+}
+
+extension Object {
+    /// Добавляем модель в БД без первичного ключа
+    func add() {
+        let realm = try? Realm()
+        try? realm?.write {
+            realm?.add(self)
+        }
+    }
+
+    /// Добавление модели в БД с первичным ключом
+    func addWithPrimaryKey() {
+        let realm = try? Realm()
+        try? realm?.write {
+            realm?.add(self, update: .all)
+        }
+    }
+
+    /// Обновление модели в БД
+    ///
+    /// - Parameter updateBlock: внутри обновляемый объект
+    func update(updateBlock: () -> Void) {
+        let realm = try? Realm()
+        try? realm?.write(updateBlock)
+    }
+
+    /// Удаление объекта из БД
+    func delete() {
+        let realm = try? Realm()
+        try? realm?.write {
+            realm?.delete(self)
+        }
+    }
+}
+
+/// способ каскадного удаления
+protocol CascadeDeleting {
+    func delete<S: Sequence>(_ objects: S, cascading: Bool) where S.Iterator.Element: Object
+    func delete<Entity: Object>(_ entity: Entity, cascading: Bool)
+}
+
+extension Realm: CascadeDeleting {
+    func delete<S: Sequence>(_ objects: S, cascading: Bool) where S.Iterator.Element: Object {
+        for obj in objects {
+            delete(obj, cascading: cascading)
+        }
+    }
+
+    func delete<Entity: Object>(_ entity: Entity, cascading: Bool) {
+        if cascading {
+            cascadeDelete(entity)
+        }
+        else {
+            delete(entity)
+        }
+    }
+}
+
+private extension Realm {
+
+    private func cascadeDelete(_ entity: RLMObjectBase) {
+        guard
+            let entity = entity as? Object else {
+            return
+        }
+        var toBeDeleted = Set<RLMObjectBase>()
+        toBeDeleted.insert(entity)
+        while !toBeDeleted.isEmpty {
+            guard
+                let element = toBeDeleted.removeFirst() as? Object,
+                !element.isInvalidated else {
+                continue
+            }
+            resolve(element: element, toBeDeleted: &toBeDeleted)
+        }
+    }
+
+    private func resolve(element: Object, toBeDeleted: inout Set<RLMObjectBase>) {
+        element.objectSchema.properties.forEach {
+            guard
+                let value = element.value(forKey: $0.name) else {
+                return
+            }
+            if let entity = value as? RLMObjectBase {
+                toBeDeleted.insert(entity)
+            }
+            else if let list = value as? RealmSwift.ListBase {
+                for index in 0 ..< list._rlmArray.count {
+                    if let realmObject = list._rlmArray.object(at: index) as? RLMObjectBase {
+                        toBeDeleted.insert(realmObject)
+                    }
+                }
+            }
+        }
+        delete(element)
+    }
+}
