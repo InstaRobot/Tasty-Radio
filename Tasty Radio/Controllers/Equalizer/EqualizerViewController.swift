68ac3c86de3ea7ae6c3030aa61acb0ecb7b3d439
Vitaliy Podolskiy <instarobot.net@gmail.com>
2021-01-24 02:06:59 +0300

    add empty equalizer controller

---
+++ Tasty Radio/Controllers/Equalizer/EqualizerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EqualizerViewController.swift
+//  Tasty Radio
+//
+//  Created by Vitaliy Podolskiy on 24.01.2021.
+//  Copyright © 2021 DEVLAB, LLC. All rights reserved.
+//
+
+import UIKit
+
+class EqualizerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
